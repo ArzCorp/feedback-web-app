e4af4cdd80db9a0c48f5902ab32532e2560de60c
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-10-03 21:55:18 -0500

    💄 added pill component

---
+++ src/components/Pill.js
@@ -0,0 +1,45 @@
+import styles from 'styles/components/pill.module.css'
+import arrowIcon from 'assets/icon-arrow.svg'
+
+import { useState } from 'react'
+import Image from 'next/image'
+
+export default function Pill({
+	children,
+	value,
+	variant,
+	handleClick = () => {},
+}) {
+	const [isActive, setIsActive] = useState(false)
+
+	const activeDefaultStyles = isActive ? styles.pillDefaultActive : ''
+
+	const variantStyles =
+		variant === 'icon' ? styles.pillVariantIcon : styles.pillDefault
+
+	const iconActiveStyles = isActive ? styles.pillIconActive : ''
+
+	return (
+		<div
+			className={`${activeDefaultStyles} ${styles.pill} ${variantStyles} text-xs`}
+			onClick={() => {
+				setIsActive(!isActive)
+				if (isActive) return handleClick(value)
+				return handleClick('')
+			}}
+		>
+			{variant === 'icon' ? (
+				<Image
+					className={`${iconActiveStyles} ${styles.pillIcon}`}
+					src={arrowIcon}
+					width={14}
+					height={10}
+					alt="Flecha hacia arriba"
+				/>
+			) : (
+				''
+			)}
+			<p className={styles.pillText}>{children}</p>
+		</div>
+	)
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,14 @@
 import styles from 'styles/pages/home.module.css'
 
 import Button from 'components/Button'
+import Pill from 'components/Pill'
 
 export default function Home() {
 	return (
 		<div className={styles.container}>
-			<title>Home</title>
-			<h1 className={styles.title}>
-				Bienvenido a <span className="titleStrong">Feedback app</span>
-			</h1>
-			<span className={styles.divider}></span>
-			<h1>Titulo h1</h1>
-			<h2>Titulo h2</h2>
-			<h3>Titulo h3</h3>
-			<h4>Titulo h4</h4>
-			<br />
-			<p className="text-regular">Texto regular</p>
-			<p className="text-small">Texto small</p>
-			<p className="text-xs">Texto extra small</p>
-			<span className={styles.divider}></span>
-			<Button type="button" variant="back-dark">
-				Go back
-			</Button>
+			<Button type="button">Go back</Button>
+			<Pill variant="icon">99</Pill>
+			<Pill>UX</Pill>
 		</div>
 	)
 }
