67fd5812b908ab95554ab3f1828da88cc59fbc0d
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-10-07 21:13:57 -0500

    🍱 added mocks

--- src/utils/mock.js
+++ src/utils/mock.js
@@ -12,3 +12,47 @@ export const OPTIONS = [
 		value: 'Most Comments',
 	},
 ]
+
+export const TAGS = [
+	{
+		id: 1,
+		name: 'Feature',
+	},
+	{
+		id: 2,
+		name: 'Enhancement',
+	},
+	{
+		id: 3,
+		name: 'Bug',
+	},
+	{
+		id: 4,
+		name: 'UI',
+	},
+	{
+		id: 5,
+		name: 'UX',
+	},
+]
+
+export const STATUS = [
+	{
+		id: 1,
+		name: 'Pendiente',
+		color: '#F49F85',
+		task: 0,
+	},
+	{
+		id: 2,
+		name: 'En progreso',
+		color: '#AD1FEA',
+		task: 0,
+	},
+	{
+		id: 3,
+		name: 'Completado',
+		color: '#62BCFA',
+		task: 0,
+	},
+]
