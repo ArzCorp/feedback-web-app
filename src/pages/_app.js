daba0533109514bccaaf1c3f905ea00973a94b68
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-10-07 19:34:27 -0500

    ➕ added icon from fontawesome

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,17 @@
+import Script from 'next/script'
+import { Fragment } from 'react'
 import 'styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />
+	return (
+		<Fragment>
+			<Script
+				src="https://kit.fontawesome.com/3d9c3c44c5.js"
+				crossorigin="anonymous"
+			></Script>
+			<Component {...pageProps} />
+		</Fragment>
+	)
 }
 
 export default MyApp
