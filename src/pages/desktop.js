c71070b8d116d83f34d11f9025875202d4eedba3
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-10-07 21:14:29 -0500

    🚧 working in desktop page

--- src/pages/desktop.js
+++ src/pages/desktop.js
@@ -1,10 +1,35 @@
 import Header from 'components/Header'
 import Layout from 'components/Layout'
+import Pill from 'components/Pill'
+
+import styles from 'styles/pages/desktop.module.css'
+import { TAGS } from 'utils/mock'
 
 export default function Desktop() {
 	return (
-		<Layout>
-			<Header />
+		<Layout twoColumns>
+			<section className={styles.desktopSidebar}>
+				<header className={styles.desktopLogo}>
+					<h2>Feedback app</h2>
+					<p className="text-small">Board</p>
+				</header>
+				<div className={styles.desktopTags}>
+					<Pill>Todos</Pill>
+					{TAGS.map((tag) => (
+						<Pill key={tag.id}>{tag.name}</Pill>
+					))}
+				</div>
+				<div className={styles.desktopRoadMap}>
+					<header>
+						<h3>Roadmap</h3>
+						<p className={` text-xs`}>View</p>
+					</header>
+					<div></div>
+				</div>
+			</section>
+			<section>
+				<Header />
+			</section>
 		</Layout>
 	)
 }
