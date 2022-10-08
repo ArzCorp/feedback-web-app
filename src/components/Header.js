aa742b4fb6c4e969e786ef12218b99b61c9a178b
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-10-07 20:29:31 -0500

    💄 added header component

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import { OPTIONS } from 'utils/mock'
+import Button from './Button'
+import Dropdown from './Dropdown'
+import styles from 'styles/components/header.module.css'
+
+export default function Header() {
+	const [currentSuggestions, setCurrentSuggestions] = useState(0)
+	const [currentFilter, setCurrentFilter] = useState('')
+	return (
+		<header className={styles.header}>
+			<div className={styles.headerDetails}>
+				<h3>
+					<i className="fa-solid fa-lightbulb"></i>{' '}
+					{`${currentSuggestions} Sugerencias`}
+				</h3>
+				<Dropdown
+					label="Ordenar por: "
+					strongLabel={currentFilter}
+					options={OPTIONS}
+					handleSelectedChange={(selected) => {
+						console.log(selected)
+						setCurrentFilter(selected.text)
+					}}
+				/>
+			</div>
+			<Button icon="fa-solid fa-plus">Añadir</Button>
+		</header>
+	)
+}
