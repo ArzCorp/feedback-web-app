ab5d78e606c96328c7428c46f0fb1978a47a6a58
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-10-08 08:56:08 -0500

    💄 added empty feedback list

--- src/pages/desktop.js
+++ src/pages/desktop.js
@@ -1,9 +1,13 @@
+import Button from 'components/Button'
 import Header from 'components/Header'
 import Layout from 'components/Layout'
 import Pill from 'components/Pill'
+import Image from 'next/image'
+
+import emptyListImage from 'assets/empty-list-image.png'
 
 import styles from 'styles/pages/desktop.module.css'
-import { TAGS } from 'utils/mock'
+import { STATUS, TAGS } from 'utils/mock'
 
 export default function Desktop() {
 	return (
@@ -24,11 +28,46 @@ export default function Desktop() {
 						<h3>Roadmap</h3>
 						<p className={` text-xs`}>View</p>
 					</header>
-					<div></div>
+					<div className={styles.desktopStatus}>
+						{STATUS.map((state) => {
+							return (
+								<div
+									key={state.id}
+									className={`${styles.desktopStatusItem} text-regular`}
+								>
+									<p>
+										<span
+											className={styles.desktopStatusItemCircle}
+											style={{ backgroundColor: state.color }}
+										></span>
+										{` ${state.name}`}
+									</p>
+									<p className={styles.desktopStatusItemNumber}>{state.task}</p>
+								</div>
+							)
+						})}
+					</div>
 				</div>
 			</section>
 			<section>
 				<Header />
+				<div className={styles.desktopFeedbackList}>
+					<div className={styles.desktopFeedbackListEmpty}>
+						<Image
+							src={emptyListImage}
+							width={129}
+							height={136}
+							alt="No tienes comentarios aun"
+						/>
+						<h1>Aún no hay comentarios.</h1>
+						<p className="text-regular">
+							¿Tienes una sugerencia? ¿Encontró un error que necesita ser
+							aplastado? Nos encanta escuchar nuevas ideas para mejorar nuestra
+							aplicación.
+						</p>
+						<Button icon="fa-solid fa-plus">Añadir</Button>
+					</div>
+				</div>
 			</section>
 		</Layout>
 	)
