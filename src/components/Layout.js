60266827a00ea722fd5eda73ad9de1c26d0b1b98
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-10-07 21:12:54 -0500

    💄 added layout type

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,13 @@
 import styles from 'styles/components/layout.module.css'
 
-export default function Layout({ children }) {
+export default function Layout({ children, twoColumns }) {
+	const layoutType = twoColumns ? styles.twoColumnsLayout : ''
+
 	return (
 		<main className={styles.layout}>
-			<div className={styles.layoutContainer}>{children}</div>
+			<div className={`${styles.layoutContainer} ${layoutType}`}>
+				{children}
+			</div>
 		</main>
 	)
 }
